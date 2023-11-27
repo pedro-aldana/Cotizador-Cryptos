bcaf1c09084d11d9d98280b2fc32ddf335b36ca4
pedro-aldana <pedroaldana987@gmail.com>
2023-11-27 10:47:59 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+import Error from "./Error";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+
+function Formulario({ setMonedas }) {
+  const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
+
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        const objecto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+
+        return objecto;
+      });
+
+      setCriptos(arrayCriptos);
+    };
+
+    consultarAPI();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+    }
+
+    setError(false);
+    setMonedas({
+      moneda,
+      criptomoneda,
+    });
+  };
+
+  return (
+    <>
+      {error && <Error>Todos los Campos son Obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomoneda />
+
+        {moneda}
+
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
+  );
+}
+
+export default Formulario;
---
+++ src/components/Resultado.jsx
@@ -0,0 +1,62 @@
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Texto = styled.p`
+  font-size: 15px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 20px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+function Resultado({ resultado }) {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+  return (
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="Imagen Cripto"
+      />
+      <div>
+        <Precio>
+          El Precio es de: <span>{PRICE}</span>{" "}
+        </Precio>
+        <Texto>
+          Precio mas alto del dia: <span> {HIGHDAY}</span>{" "}
+        </Texto>
+        <Texto>
+          Precio mas bajo del dia: <span> {LOWDAY}</span>{" "}
+        </Texto>
+        <Texto>
+          Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>{" "}
+        </Texto>
+        <Texto>
+          Ultima Actualizacion: <span> {LASTUPDATE}</span>{" "}
+        </Texto>
+      </div>
+    </Contenedor>
+  );
+}
+
+export default Resultado;
